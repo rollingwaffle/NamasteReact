c8dedaa05ea67b3cc5054d3e96b63e6070f64175
Roshan <roshanrazz30@gmail.com>
2024-04-17 12:02:34 +0530

    session1

---
+++ session_1/app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{style : {color : "red",backgroundColor : "black"} },"Hello world");
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
