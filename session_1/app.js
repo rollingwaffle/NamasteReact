d927985bbfb6c7ed513ce2f2f984918440cacd86
Roshan <roshanrazz30@gmail.com>
2024-04-17 12:18:37 +0530

    changes updated

--- session_1/app.js
+++ session_1/app.js
@@ -1,5 +1,9 @@
+
+
 const heading = React.createElement("h1",{style : {color : "red",backgroundColor : "black"} },"Hello world");
+const heading2 = React.createElement("h2",{style : {color : "red"}},"Namaste React")
+const div = React.createElement("div",{},[heading,heading2])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(div);
